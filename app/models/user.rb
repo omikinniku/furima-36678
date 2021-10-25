d0a22b3791b007b22bab5aaf4bf341e944a5801c
omikinniku <shijiikuomi@gmail.com>
2021-10-25 17:37:39 +0900

    Userのモデル単体テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u} # 全角カナ
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u}
   validates :birthday, presence: true
 end
