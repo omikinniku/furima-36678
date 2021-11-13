3a472538a82a0edb2f8bf00f4e123a42330b0b3d
omikinniku <shijiikuomi@gmail.com>
2021-11-13 15:59:06 +0900

    Formオブジェクトパターンでの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   
   def index
     @item = Item.find(params[:item_id])
+    @order_buyer = OrderBuyer.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order_buyer = OrderBuyer.new(order_params)
+    if @order_buyer.valid? #ApplicationRecordを継承していないことにより、saveメソッドにはバリデーションを実行する機能がないため
+      @order_buyer.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
-
+  def order_params
+    params.require(:order_buyer).permit(
+      :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
+    ).merge(user_id: current_user.id, item_id: @item.id)
+  end
 end
---
+++ app/models/order_buyer.rb
@@ -0,0 +1,21 @@
+class OrderBuyer
+  # Formオブジェクト使用
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :item_id, :user_id
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :city
+    validates :address
+    validates :phone_number, format: {with: /\A[0-9]{11}\z/ }
+    validates :item_id
+    validates :user_id
+  end
+  validates :prefecture_id, numericality: {other_than: 1, message: "選択してください"}
+
+  def save
+    # 各テーブルにデータを保存する処理
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Buyer.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
+end
